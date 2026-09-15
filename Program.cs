2746397dde8bcd5f9cd8b2c5f06f5461598f729c
Luis <luisjasper94@gmail.com>
2026-09-15 18:40:11 +0800

    first

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Guess
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine ("Hello test");
+        }
+    }
+}
